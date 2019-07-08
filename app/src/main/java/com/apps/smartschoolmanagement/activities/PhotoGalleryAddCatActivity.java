c96b9cf8978b21402dda805e6f72c6d488920dc3
ronak  suvagiya <ronaksuvagiya40@gmail.com>
2019-07-08 09:21:02 +0530

    + implement add image category

--- app/src/main/java/com/apps/smartschoolmanagement/activities/PhotoGalleryAddCatActivity.java
+++ app/src/main/java/com/apps/smartschoolmanagement/activities/PhotoGalleryAddCatActivity.java
@@ -3,19 +3,33 @@ package com.apps.smartschoolmanagement.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.apps.smartschoolmanagement.Comman.URL;
 import com.apps.smartschoolmanagement.R;
+import com.apps.smartschoolmanagement.utils.URLs;
 import com.kaopiz.kprogresshud.KProgressHUD;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PhotoGalleryAddCatActivity extends AppCompatActivity {
     private EditText Cat;
     private Button uploadCat;
-
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +39,9 @@ public class PhotoGalleryAddCatActivity extends AppCompatActivity {
         Cat = (EditText) findViewById(R.id.edtCat);
         uploadCat = (Button) findViewById(R.id.btnSave);
 
+        sp = PreferenceManager.getDefaultSharedPreferences(PhotoGalleryAddCatActivity.this);
+        String channel = (sp.getString("schoolid", ""));
+        int schoolid = Integer.parseInt(channel);
         uploadCat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +50,35 @@ public class PhotoGalleryAddCatActivity extends AppCompatActivity {
                     Cat.requestFocus();
                     return;
                 } else {
-                    Toast.makeText(PhotoGalleryAddCatActivity.this,"save successfully!",Toast.LENGTH_LONG).show();
+                    RequestQueue MyRequestQueue = Volley.newRequestQueue(PhotoGalleryAddCatActivity.this);
+                    JSONObject jsonBody = new JSONObject();
+                    try {
+                            jsonBody.put("name", Cat.getText().toString().trim());
+                            jsonBody.put("school",schoolid);
+
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URLs.addImgCategory, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String success = String.valueOf(response.get("message"));
+                                Toast.makeText(PhotoGalleryAddCatActivity.this, success, Toast.LENGTH_LONG).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            finish();
+                        }
+                    },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(PhotoGalleryAddCatActivity.this, "Enter Category", Toast.LENGTH_LONG).show();
+
+                                }
+                            });
+                            MyRequestQueue.add(jsonObjectRequest);
+                    } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                 }
             }
         });
